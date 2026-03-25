2fa80e5b94a0444eb04c256ff5d837390dd8225f
Thomas Zoeter <thomas.zoeter@keylane.com>
2026-03-25 11:35:09 +0100

    Remove /game redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import {GameView} from './game-view/game-view'
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/game', pathMatch: 'full'},
-  { path: 'game', component: GameView },
+  { path: '', component: GameView },
 ];
