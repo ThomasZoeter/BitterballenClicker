008b3e9e1ac421bf72698723a32d236f6f72392c
Thomas Zoeter <thomas.zoeter@keylane.com>
2026-03-27 13:12:55 +0100

    Add transform until decillion (10e33)

--- src/app/game-view/BB-screen-component/BB-background-component/BB-component/BB-component.ts
+++ src/app/game-view/BB-screen-component/BB-background-component/BB-component/BB-component.ts
@@ -56,9 +56,37 @@ export class BBComponent implements OnDestroy, OnInit{
     if(num > 1000) {
       result = num / 1000 + " thousand"
     }
-    if(num > 1000000)  {
+    if(num > Math.pow(10,6))  {
       result = Math.floor(num / 1000) / 1000 + " million" // In parts to make sure there are 3 digits
     }
+    if(num > Math.pow(10,9))  {
+      result = Math.floor(num / Math.pow(10,6)) / 1000 + " billion"
+    }
+    if(num > Math.pow(10,12))  {
+      result = Math.floor(num / Math.pow(10,9)) / 1000 + " trillion"
+    }
+    if(num > Math.pow(10,15))  {
+      result = Math.floor(num / Math.pow(10,12)) / 1000 + " quadrillion"
+    }
+    if(num > Math.pow(10,18))  {
+      result = Math.floor(num / Math.pow(10,15)) / 1000 + " quintillion"
+    }
+    if(num > Math.pow(10,21))  {
+      result = Math.floor(num / Math.pow(10,18)) / 1000 + " sextillion"
+    }
+    if(num > Math.pow(10,24))  {
+      result = Math.floor(num / Math.pow(10,21)) / 1000 + " septillion"
+    }
+    if(num > Math.pow(10,27))  {
+      result = Math.floor(num / Math.pow(10,24)) / 1000 + " octillion"
+    }
+    if(num > Math.pow(10,30))  {
+      result = Math.floor(num / Math.pow(10,27)) / 1000 + " nonillion"
+    }
+    if(num > Math.pow(10,33))  {
+      result = Math.floor(num / Math.pow(10,30)) / 1000 + " decillion"
+    }
+
 
     return result;
   }
